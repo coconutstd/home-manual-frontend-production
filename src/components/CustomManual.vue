47feacd87090fb4aab4eac73471f225b8b93f488
junui <wnsspdl@naver.com>
2020-11-12 21:52:57 +0900

    개인모음집 2

--- src/components/CustomManual.vue
+++ src/components/CustomManual.vue
@@ -1,18 +1,34 @@
 <template>
   <div>
-
+    <div class="custom-wrapper">
+      <div class="custom">
+        <div class="custom-header">
+          <span class="custom-title">{{ email }}</span>
+        </div>
+        <div class="list-section-wrapper">
+          <div class="list-section">
+            <div class="list-wrapper" v-for="list in custom.group_lists" :key="list.id">
+              <List :data="list"></List>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import {mapActions, mapState} from 'vuex'
+import List from './List.vue'
 
 export default {
   components: {
+    List
   },
   computed: {
     ...mapState({
-      custom: 'custom'
+      custom: 'custom',
+      email: 'email'
     })
   },
   created () {
@@ -27,5 +43,40 @@ export default {
 </script>
 
 <style>
-
+.custom-wrapper{
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+.custom{
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.custom-header{
+  flex: none;
+  padding: 8px 4px 8px 8px;
+  margin: 0;
+}
+.list-section-wrapper{
+  flex-grow: 1;
+  position: relative;
+}
+.list-section{
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 0 10px;
+}
+.list-wrapper{
+  display: inline-block;
+  height: 100%;
+  width: 272px;
+  vertical-align: top;
+  margin-right: 5px;
+}
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="list">
+    <div class="list-header">
+      <div class="list-header-title">{{ data.title }}</div>
+    </div>
+    <AddCard></AddCard>
+  </div>
+</template>
+
+<script>
+import AddCard from './AddCard'
+export default {
+  components: {AddCard},
+  props: ['data']
+}
+</script>
+
+<style>
+.list {
+  background-color: #e2e4e6;
+  border-radius: 3px;
+  margin-right: 10px;
+  display: flex;
+  flex-direction: column;
+  vertical-align: top;
+  width: 100%;
+  max-height: 100%;
+}
+
+.list-header{
+  flex: 0 0 auto;
+  padding: 10px 8px 8px;
+  position: relative;
+}
+
+.list-header-title{
+  font-family: "omni_020";
+}
+
+.add-card-btn {
+  flex: 0 0 auto;
+  display: block;
+  padding: 8px 10px;
+  color: #8c8c8c;
+  text-decoration: none;
+}
+.add-card-btn:focus,
+.add-card-btn:hover {
+  background-color: rgba(0,0,0, .1);
+}
+</style>
